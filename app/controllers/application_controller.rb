ac333be4f687dcbff9897a832a82bbe60ea84f3f
febincloudyuga <febin@Febins-MacBook-Air.local>
2019-02-12 09:31:27 +0530

    add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render_html: "hello, world!"
+    render html: "hello, world!"
+  end
 end
